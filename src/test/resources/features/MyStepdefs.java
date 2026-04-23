d912b6eaf58aa05928e1e5d47b5ff7ec0efeb282
JUAN DAVID CAPERA CASTRILLON <caperaju@HILDEBOG.LOCAL>
2026-04-23 16:20:29 -0500

    ajuste de configuración

---
+++ src/test/resources/features/MyStepdefs.java
@@ -0,0 +1,8 @@
+public class MyStepdefs {
+    public MyStepdefs() {
+        Then("^VERIFICA REDIRECCION A CLAROBOT$", () -> {
+            // Write code here that turns the phrase above into concrete actions
+            throw new io.cucumber.java.PendingException();
+        });
+    }
+}
